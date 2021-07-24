c57f3d48691638c541bda62db204730450a09916
Kenneth Johnson C <kennojc@gmail.com>
2021-07-24 19:33:39 -0400

    Modified Wines' index to show strains

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -2,4 +2,13 @@ class Wine < ApplicationRecord
     has_many :wine_strains
     has_many :strains, through: :wine_strains, dependent: :destroy
     accepts_nested_attributes_for :strains, :wine_strains, reject_if: :all_blank, allow_destroy: true
+
+    def recipe
+        @percentages = Array.new
+        self.wine_strains.each do |winestrain| 
+            @percentages.push("#{winestrain.strain.name} " + "(#{winestrain.percentage}%)") 
+        end
+        @percentages.sort.join(', ')
+    end
+
 end
