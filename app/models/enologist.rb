5b4ed56726a30be032acaf193e143f314476188b
Kenneth Johnson C <kennojc@gmail.com>
2021-07-25 23:00:11 -0400

    Ready to modify the views

--- app/models/enologist.rb
+++ app/models/enologist.rb
@@ -1,2 +1,6 @@
 class Enologist < ApplicationRecord
+    has_many :enologist_magazines
+    has_many :magazines, through: :enologist_magazines, dependent: :destroy
+    has_many :wine_enologists
+    has_many :wines, through: :wine_enologists, dependent: :destroy
 end
